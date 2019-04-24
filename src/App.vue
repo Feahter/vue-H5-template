80af195b424ff0675a23427a327c141b630770b2
“feahter” <fuzhuo@fuzhuodeMacBook-Pro.local>
2019-04-24 18:09:26 +0800

    add content animate

--- src/App.vue
+++ src/App.vue
@@ -34,6 +34,69 @@ export default {
     }
   },
   methods: {
+    each (object, callback) {
+      let type = (function () {
+        switch (object.constructor) {
+          case Object:
+            return 'Object'
+            break
+          case Array:
+            return 'Array'
+            break
+          case NodeList:
+            return 'NodeList'
+            break
+          default:
+            return 'null'
+            break
+        }
+      })()
+      if (type === 'Array' || type === 'NodeList') {
+        [].every.call(
+          object, function (v, i) {
+            return callback.call(v, i, v) !== false
+          })
+      } else if (type === 'Object') {
+        for (var i in object) {
+          if (callback.call(object[i], i, object[i]) === false) {
+            break
+          }
+        }
+      }
+    },
+    initAnimationItems () {
+      let that = this
+      let items = document.querySelectorAll('.swiper-container .animated')
+      that.each(items, (i, e) => {
+        let name = e.getAttribute('data-name')
+        let duration = e.getAttribute('data-duration')
+        let delay = e.getAttribute('data-delay')
+        e.classList.add(name)
+        e.style.visibility = 'hidden'
+        e.style.transitionDuration = duration
+        e.style.transitionDelay = delay
+        // console.log(val.style, name, duration, delay)
+      })
+    },
+    playAnimation (swiper) {
+      let that = this
+      that.clearAnimation()
+      let items = swiper.slides[swiper.activeIndex].querySelectorAll('.swiper-container .animated')
+      that.each(items, (i, e) => {
+        let name = e.getAttribute('data-name')
+        e.classList.add(name)
+        e.style.visibility = 'visible'
+      })
+    },
+    clearAnimation () {
+      let that = this
+      let items = document.querySelectorAll('.swiper-container .animated')
+      that.each(items, (i, e) => {
+        let name = e.getAttribute('data-name')
+        e.classList.remove(name)
+        e.style.visibility = 'hidden'
+      })
+    },
     InitSwiper () {
       let that = this
       let mySwiper = new Swiper('.swiper-container', {
@@ -45,18 +108,25 @@ export default {
         width: window.innerWidth,
         on: {
           init () {
-            setTimeout(() => { that.showload = false }, 1000)
+            setTimeout(() => {
+              that.showload = false
+              that.initAnimationItems()
+              that.playAnimation(this)
+            }, 500)
           },
-          resize () {
-            this.params.width = window.innerWidth
-            this.params.height = window.innerHeight
-            this.update()
+          // resize () {
+          //   this.params.width = window.innerWidth
+          //   this.params.height = window.innerHeight
+          //   this.update()
+          // },
+          transitionEnd () {
+            that.playAnimation(this)
           },
           slideChange () {
             that.active = this.activeIndex
             if (this.activeIndex) {
               that.showarrow = false
-            }else {
+            } else {
               that.showarrow = true
             }
           }
--- src/pages/demo.vue
+++ src/pages/demo.vue
@@ -1,29 +1,143 @@
 <template>
-<!-- Swiper -->
+  <!-- Swiper -->
   <div class="swiper-container">
     <div class="swiper-wrapper">
-      <div class="swiper-slide" :style="'background-color:'+nowcolor" v-for="(item,index) in 5" :key="index">
-          Slide {{index}}
-          </div>
+      <div
+        class="swiper-slide"
+        :style="'background-color:'+nowcolor"
+        v-for="(item,index) in 5"
+        :key="index"
+      >
+        <p
+          class="item-text animated"
+          data-name="slideInLeft"
+          data-duration="1s"
+          data-delay="0.5s"
+        >Slide {{index}}</p>
+        <p
+          class="item-text animated"
+          data-name="slideInLeft"
+          data-duration="1s"
+          data-delay="0.5s"
+        >I'm a coder!</p>
+      </div>
+      <!-- <div class="swiper-slide" :style="'background-color:'+nowcolor">
+        <p class="item-text animated"  data-name="slideInLeft" data-duration="1s" data-delay="0.5s">Slide 1</p>
+        <p class="item-text animated" data-name="slideInLeft" data-duration="1s" data-delay="0.5s">I'm a coder!</p>
+      </div>
+        <div class="swiper-slide" :style="'background-color:'+nowcolor">
+        <p class="item-text animated"  data-name="slideInRight" data-duration="1s" data-delay="0.5s">Slide 2</p>
+        <p class="item-text animated" data-name="slideInRight" data-duration="1s" data-delay="0.5s">I'm a coder!</p>
+      </div>
+        <div class="swiper-slide" :style="'background-color:'+nowcolor">
+        <p class="item-text animated"  data-name="slideInUp" data-duration="1s" data-delay="0.5s">Slide 3</p>
+        <p class="item-text animated" data-name="slideInUp" data-duration="1s" data-delay="0.5s">I'm a coder!</p>
+      </div>
+      <div class="swiper-slide" :style="'background-color:'+nowcolor">
+        <p class="item-text animated"  data-name="slideInDown" data-duration="1s" data-delay="0.5s">Slide 4</p>
+        <p class="item-text animated" data-name="slideInDown" data-duration="1s" data-delay="0.5s">I'm a coder!</p>
+      </div>-->
     </div>
-<!-- music -->
-<Music/>
-</div>
+    <!-- music -->
+    <Music/>
+  </div>
 </template>
 
 <script>
 import Music from '../components/music'
 export default {
-  props: [ 'active' ],
+  props: ['active'],
   components: { Music },
   data () {
     return {
-      nowcolor: '#000'
+      nowcolor: '#000',
+      arrClass: [
+        'bounce',
+        'flash',
+        'pulse',
+        'rubberBand',
+        'shake',
+        'headShake',
+        'swing',
+        'tada',
+        'wobble',
+        'jello',
+        'bounceIn',
+        'bounceInDown',
+        'bounceInLeft',
+        'bounceInRight',
+        'bounceInUp',
+        'bounceOut',
+        'bounceOutDown',
+        'bounceOutLeft',
+        'bounceOutRight',
+        'bounceOutUp',
+        'fadeIn',
+        'fadeInDown',
+        'fadeInDownBig',
+        'fadeInLeft',
+        'fadeInLeftBig',
+        'fadeInRight',
+        'fadeInRightBig',
+        'fadeInUp',
+        'fadeInUpBig',
+        'fadeOut',
+        'fadeOutDown',
+        'fadeOutDownBig',
+        'fadeOutLeft',
+        'fadeOutLeftBig',
+        'fadeOutRight',
+        'fadeOutRightBig',
+        'fadeOutUp',
+        'fadeOutUpBig',
+        'flipInX',
+        'flipInY',
+        'flipOutX',
+        'flipOutY',
+        'lightSpeedIn',
+        'lightSpeedOut',
+        'rotateIn',
+        'rotateInDownLeft',
+        'rotateInDownRight',
+        'rotateInUpLeft',
+        'rotateInUpRight',
+        'rotateOut',
+        'rotateOutDownLeft',
+        'rotateOutDownRight',
+        'rotateOutUpLeft',
+        'rotateOutUpRight',
+        'hinge',
+        'jackInTheBox',
+        'rollIn',
+        'rollOut',
+        'zoomIn',
+        'zoomInDown',
+        'zoomInLeft',
+        'zoomInRight',
+        'zoomInUp',
+        'zoomOut',
+        'zoomOutDown',
+        'zoomOutLeft',
+        'zoomOutRight',
+        'zoomOutUp',
+        'slideInDown',
+        'slideInLeft',
+        'slideInRight',
+        'slideInUp',
+        'slideOutDown',
+        'slideOutLeft',
+        'slideOutRight',
+        'slideOutUp',
+        'heartBeat'
+      ]
     }
   },
   methods: {
     getColor () {
       return '#' + Math.floor(Math.random() * 0xffffff).toString(16)
+    },
+    getAnimate () {
+      console.log(111)
     }
   },
   watch: {
@@ -38,15 +152,15 @@ export default {
 </script>
 
 <style>
-.swiper-container{
+.swiper-container {
   max-height: 100vh;
-  overflow:hidden;
+  overflow: hidden;
 }
-.swiper-slide{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: #fff;
-    font-size: 30px;
+.swiper-slide {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  font-size: 30px;
 }
 </style>
