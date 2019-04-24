0a6f2fbb6d42be84f80b98e2e1e1c7f2563672c1
“feahter” <fuzhuo@fuzhuodeMacBook-Pro.local>
2019-04-24 10:22:31 +0800

    add swiper

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <Demo :active="active"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Demo from '@/components/demo'
+import Swiper from 'swiper'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Demo
+  },
+  data () {
+    return {
+      active: ''
+    }
+  },
+  methods: {
+    InitSwiper () {
+      let that = this
+      let mySwiper = new Swiper('.swiper-container', {
+        direction: 'vertical',
+        autoplay: true,
+        // loop: true,
+        // effect: 'cube',
+        height: window.innerHeight,
+        width: window.innerWidth,
+        on: {
+          resize () {
+            this.params.width = window.innerWidth
+            this.params.height = window.innerHeight
+            this.update()
+          },
+          slideChange () {
+            that.active = this.activeIndex
+          }
+        }
+      })
+    }
+  },
+  mounted () {
+    this.InitSwiper()
   }
 }
 </script>
 
 <style>
+@import url('../node_modules/swiper/dist/css/swiper.min.css');
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+body{
+  margin: 0;
 }
 </style>
---
+++ src/components/demo.vue
@@ -0,0 +1,46 @@
+<template>
+<!-- Swiper -->
+  <div class="swiper-container">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide" :style="'background-color:'+nowcolor" v-for="(item,index) in 5" :key="index">Slide {{index}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [ 'active' ],
+  data () {
+    return {
+      nowcolor: ''
+    }
+  },
+  methods: {
+    getColor () {
+      return '#' + Math.floor(Math.random() * 0xffffff).toString(16)
+    }
+  },
+  watch: {
+    active (cur, old) {
+      let that = this
+      if (cur !== old) {
+        that.nowcolor = that.getColor()
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.swiper-container{
+  max-height: 100vh;
+  overflow:hidden;
+}
+.swiper-slide{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    font-size: 30px;
+}
+</style>
