4344efec2eb7b449545796bd31c98497bb5b647a
“feahter” <fuzhuo@fuzhuodeMacBook-Pro.local>
2019-04-25 13:46:28 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -2,166 +2,166 @@
   <div id="app">
     <!-- loading -->
     <transition
-      appear
-      enter-active-class="animated fadeIn"
-      leave-active-class="animated fadeOut"
-      appear-class="animated swing"
-      :duration="3000"
-    >
-      <Loading v-if="showload"/>
-    </transition>
-    <Demo :active="active"/>
-    <Arrow v-if="showarrow"/>
+    appear
+    enter-active-class="animated fadeIn"
+    leave-active-class="animated fadeOut"
+    appear-class="animated swing"
+    :duration="3000">
+    <Loading v-if="showload"/>
+      </transition>
+   <Demo :active="active" />
+<Arrow v-if="showarrow"/>
   </div>
 </template>
 
 <script>
-import Loading from "@/components/loading";
-import Arrow from "@/components/arrow";
-import Demo from "@/pages/demo";
-import Swiper from "swiper";
+import Loading from '@/components/loading'
+import Arrow from '@/components/arrow'
+import Demo from '@/pages/demo'
+import Swiper from 'swiper'
 export default {
-  name: "app",
+  name: 'app',
   components: {
     Loading,
     Arrow,
     Demo
   },
-  data() {
+  data () {
     return {
       showload: true,
       showarrow: true,
-      active: ""
-    };
+      active: ''
+    }
   },
   methods: {
-    each(object, callback) {
-      let type = (function() {
+    each (object, callback) {
+      let type = (function () {
         switch (object.constructor) {
           case Object:
-            return "Object";
-            break;
+            return 'Object'
+            // eslint-disable-next-line no-unreachable
+            break
           case Array:
-            return "Array";
-            break;
+            return 'Array'
+            // eslint-disable-next-line no-unreachable
+            break
           case NodeList:
-            return "NodeList";
-            break;
+            return 'NodeList'
+            // eslint-disable-next-line no-unreachable
+            break
           default:
-            return "null";
-            break;
+            return 'null'
+            // eslint-disable-next-line no-unreachable
+            break
         }
-      })();
-      if (type === "Array" || type === "NodeList") {
-        [].every.call(object, function(v, i) {
-          return callback.call(v, i, v) !== false;
-        });
-      } else if (type === "Object") {
+      })()
+      if (type === 'Array' || type === 'NodeList') {
+        [].every.call(
+          object, function (v, i) {
+            return callback.call(v, i, v) !== false
+          })
+      } else if (type === 'Object') {
         for (var i in object) {
           if (callback.call(object[i], i, object[i]) === false) {
-            break;
+            break
           }
         }
       }
     },
-    initAnimationItems() {
-      let that = this;
-      let items = document.querySelectorAll(".swiper-container .animated");
+    initAnimationItems () {
+      let that = this
+      let items = document.querySelectorAll('.swiper-container .animated')
       that.each(items, (i, e) => {
-        let name = e.getAttribute("data-name");
-        let duration = e.getAttribute("data-duration");
-        let delay = e.getAttribute("data-delay");
-        e.classList.add(name);
-        e.style.visibility = "hidden";
-        e.style.transitionDuration = duration;
-        e.style.transitionDelay = delay;
+        let name = e.getAttribute('data-name')
+        let duration = e.getAttribute('data-duration')
+        let delay = e.getAttribute('data-delay')
+        e.classList.add(name)
+        e.style.visibility = 'hidden'
+        e.style.transitionDuration = duration
+        e.style.transitionDelay = delay
         // console.log(val.style, name, duration, delay)
-      });
+      })
     },
-    playAnimation(swiper) {
-      let that = this;
-      that.clearAnimation();
-      let items = swiper.slides[swiper.activeIndex].querySelectorAll(
-        ".swiper-container .animated"
-      );
+    playAnimation (swiper) {
+      let that = this
+      that.clearAnimation()
+      let items = swiper.slides[swiper.activeIndex].querySelectorAll('.swiper-container .animated')
       that.each(items, (i, e) => {
-        let old = e.classList.item(2);
-        let name = e.getAttribute("data-name");
-        if (old) {
-          e.classList.replace(old, name);
-        } else {
-          e.classList.add(name);
+        let old = e.classList.item(2)
+        let name = e.getAttribute('data-name')
+        if (old) { e.classList.replace(old, name) } else {
+          e.classList.add(name)
         }
-        e.style.visibility = "visible";
-      });
+        e.style.visibility = 'visible'
+      })
     },
-    clearAnimation() {
-      let that = this;
-      let items = document.querySelectorAll(".swiper-container .animated");
+    clearAnimation () {
+      let that = this
+      let items = document.querySelectorAll('.swiper-container .animated')
       that.each(items, (i, e) => {
-        let old = e.classList.item(2);
-        e.classList.remove(old);
-        e.style.visibility = "hidden";
-      });
+        let old = e.classList.item(2)
+        e.classList.remove(old)
+        e.style.visibility = 'hidden'
+      })
     },
-    InitSwiper() {
-      let that = this;
-      let mySwiper = new Swiper(".swiper-container", {
-        direction: "vertical",
+    InitSwiper () {
+      let that = this
+      // eslint-disable-next-line no-unused-vars
+      let mySwiper = new Swiper('.swiper-container', {
+        direction: 'vertical',
         // autoplay: true,
         // loop: true,
-        effect: "coverflow",
+        effect: 'coverflow',
         height: window.innerHeight,
         width: window.innerWidth,
         on: {
-          init() {
+          init () {
             setTimeout(() => {
-              that.initAnimationItems();
-              that.showload = false;
-              that.playAnimation(this);
-            }, 1000);
+              that.initAnimationItems()
+              that.showload = false
+              that.playAnimation(this)
+            }, 800)
           },
-          resize() {
-            this.params.width = window.innerWidth;
-            this.params.height = window.innerHeight;
-            this.update();
+          resize () {
+            this.params.width = window.innerWidth
+            this.params.height = window.innerHeight
+            this.update()
           },
-          transitionEnd() {
-            that.playAnimation(this);
+          transitionEnd () {
+            that.playAnimation(this)
           },
-          slideChange() {
-            that.active = this.activeIndex;
+          slideChange () {
+            that.active = this.activeIndex
             if (this.activeIndex) {
-              that.showarrow = false;
+              that.showarrow = false
             } else {
-              that.showarrow = true;
+              that.showarrow = true
             }
           }
         }
-      });
+      })
     }
   },
-  mounted() {
-    let that = this;
-    that.InitSwiper();
+  mounted () {
+    let that = this
+    that.InitSwiper()
   }
-};
+}
 </script>
 
 <style>
-@import url("../node_modules/swiper/dist/css/swiper.min.css");
+@import url('../node_modules/swiper/dist/css/swiper.min.css');
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-body {
+body{
   margin: 0;
 }
-button:focus,
-i:focus {
-  outline: none;
+button:focus,i:focus{
+    outline: none;
 }
 </style>
